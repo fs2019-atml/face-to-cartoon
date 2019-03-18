df43d8309ac71440b26fc245a2e3869467a97f92
Benjamin Fankhauser <benjamin.fankhauser@bfh.ch>
2019-03-18 11:29:55 +0100

    download equations directly

---
+++ doc/images/update.sh
@@ -0,0 +1,10 @@
+!/bin/bash
+
+function get {
+	curl -o $1 $2
+}
+
+# readme images:
+get function-g.png "http://www.sciweavers.org/tex2img.php?eq=G%3A%20X%20%5Crightarrow%20Y%0A&bc=White&fc=Black&im=png&fs=12&ff=arev&edit=0"
+get function-h.png "http://www.sciweavers.org/tex2img.php?eq=H%3A%20Y%20%5Crightarrow%20X%0A&bc=White&fc=Black&im=png&fs=12&ff=arev&edit=0"
+get transitive.png "http://www.sciweavers.org/tex2img.php?eq=x%20%3D%20H%28G%28x%29%29%20%0A&bc=White&fc=Black&im=png&fs=12&ff=arev&edit=0"
