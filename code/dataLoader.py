a6af9e804e3d2b00d34003b5f1c83daa89cc4967
janistrov <jan.segessenmann@gmail.com>
2019-04-02 15:37:57 +0200

    Update dataLoader.py

--- code/dataLoader.py
+++ code/dataLoader.py
@@ -1,3 +1,45 @@
-# Loader to Numpy
-# Loader to Torch
-# Normalization
+# Something in this manner (not yet working)
+
+from torch.utils.data import Dataset, DataLoader
+from torchvision.transforms import Compose, ToTensor, Normalize
+
+class Aset(Dataset):
+    def __init__(self, images, transform):
+        self.images = images
+        self.transform = transform
+        
+    def __len__(self):
+        return self.images.shape[0]
+    
+    def __getitem__(self, idx):
+        image = self.images[idx,:]
+        if self.transform != None:
+            image = self.transform(image.reshape(None,None,3))
+        return (image)
+
+class Bset(Dataset):
+    def __init__(self, images, transform):
+        self.images = images
+        self.transform = transform
+        
+    def __len__(self):
+        return self.images.shape[0]
+    
+    def __getitem__(self, idx):
+        image = self.images[idx,:]
+        if self.transform != None:
+            image = self.transform(image.reshape(None,None,3))
+        return (image)
+
+      
+transforms = Compose([ToTensor(), Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))])
+
+trainA_set = DigitDataset(None, transforms)
+testA_set = DigitDataset(None, transforms)
+trainA_loader = DataLoader(None, batch_size=32, shuffle=True)
+testA_loader = DataLoader(None, batch_size=32, shuffle=False)
+
+trainB_set = DigitDataset(None, transforms)
+testB_set = DigitDataset(None, transforms)
+trainB_loader = DataLoader(None, batch_size=32, shuffle=True)
+testB_loader = DataLoader(None, batch_size=32, shuffle=False)
