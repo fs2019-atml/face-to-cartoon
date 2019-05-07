9b2cb6e28ec2e5dc85aaeead41e2cc5d6f3468fb
Benjamin Fankhauser <benjamin.fankhauser@bfh.ch>
2019-05-07 14:04:41 +0200

    clean face_dataset

--- code/data/face_dataset.py
+++ code/data/face_dataset.py
@@ -1,23 +1,28 @@
 import os.path
-from data.base_dataset import BaseDataset, get_transform
+from data.base_dataset import BaseDataset
 from data.image_folder import make_dataset
 from PIL import Image
 import random
 
 import torch
 import torchvision.transforms as transforms
-import numpy as np
 
 
 class UnalignedDataset(BaseDataset):
     """
-    This dataset class can load unaligned/unpaired datasets.
+    This dataset class can load unpaired face datasets.
 
-    It requires two directories to host training images from domain A '/path/to/data/trainA'
-    and from domain B '/path/to/data/trainB' respectively.
-    You can train the model with the dataset flag '--dataroot /path/to/data'.
-    Similarly, you need to prepare two directories:
-    '/path/to/data/testA' and '/path/to/data/testB' during test time.
+    It requires one dataset of real faces with the corresponding landmarks.
+    The cartoon faces have to be in the format of xx_filename.png where xx encodes
+    the class of the conditional training.
+    
+    under --dataroot we assume the following structure:
+        '099000_landmarks' a file with the landmarks of the real faces.
+        'cartoon_10k_png' a folder with all the cartoon faces.
+        
+    These choices are not the most general ones but easy to use in our context.
+
+    During test time we expect the real faces under --dataroot/test.
     """
 
     def __init__(self, opt):
@@ -27,16 +32,26 @@ class UnalignedDataset(BaseDataset):
             opt (Option class) -- stores all the experiment flags; needs to be a subclass of BaseOptions
         """
         BaseDataset.__init__(self, opt)
-        #self.dir_A = os.path.join(opt.dataroot, opt.phase + 'A')  # create a path '/path/to/data/trainA'
-        #self.A_paths = sorted(make_dataset(self.dir_A, opt.max_dataset_size))   # load images from '/path/to/data/trainA'
-
+        
+        # We assert some options:
+        assert opt.input_nc == 3
+        assert opt.output_nc == 3
+        assert opt.direction == 'AtoB'
         
         # read out landmarks files to store paths and landmarks in A:
+        
+        ## about the landmark file: ##
+        # - each line represents one image (space separated)
+        # - the first token is the relative path
+        # - the following tokens are the coordinates of 5 landmarks: <x1 y1>
+        # - the 5 landmarks are: left eye, right eye, nose, left mouth edge, right mouth edge
+        # (definition left: x1 more left x2 <=> x1 < x2, origin: top left corner)
+        #############################
+        
         self.A_paths = []
-        #with open(os.path.join(opt.dataroot, 'lfw_landmark/trainImageList.txt'), mode='r') as f:
         if (opt.phase == 'train'):
             self.A_root = os.path.join(self.opt.dataroot, '099000_landmarks')
-            with open(os.path.join(opt.dataroot, '099000_landmarks/landmarks.txt'), mode='r') as f: #use all from front selection
+            with open(os.path.join(opt.dataroot, '099000_landmarks/landmarks.txt'), mode='r') as f:
                 for line in f:
                     tokens = line.split(' ')
                     tokens[0] = '.' + tokens[0].replace('\\','/') # linux like
@@ -46,7 +61,7 @@ class UnalignedDataset(BaseDataset):
         if (opt.phase == 'test'):
             self.A_root = ''
             for path in sorted(make_dataset(os.path.join(opt.dataroot, 'test'), opt.max_dataset_size)):
-                tokens = [path, 0, 0, 0, 0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+                tokens = [path, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
                 self.A_paths.append(tokens)
         
         #self.dir_B = os.path.join(opt.dataroot, 'cartoon_orange_hair_crop')  # create a path '/path/to/data/trainB'
@@ -58,19 +73,18 @@ class UnalignedDataset(BaseDataset):
         
         print('lenghtA:', self.A_size, 'lengthB:', self.B_size)
         
-        btoA = self.opt.direction == 'BtoA'
-        input_nc = self.opt.output_nc if btoA else self.opt.input_nc       # get the number of channels of input image
-        output_nc = self.opt.input_nc if btoA else self.opt.output_nc      # get the number of channels of output image
-        self.transform_A_wide = self.get_transform_304(isBeni=True)
+        self.transform_A_wide = self.get_transform_304()
         self.transform_B_wide = self.get_transform_304(centerCrop=True)
         self.transform_A = self.get_transform()
         self.transform_B = self.get_transform()
 
-    def get_transform_304(self, centerCrop=False, isBeni=False):
+    def get_transform_304(self, centerCrop=False, isSpecial=False):
+        """
+        Transforms the input image to the "big" size of 304
+        """        
         transform_list = []
-        method=Image.BICUBIC
         
-        if (isBeni):
+        if (isSpecial):
             transform_list.append(transforms.Resize(304))
             transform_list.append(transforms.CenterCrop(304))
 
@@ -78,17 +92,19 @@ class UnalignedDataset(BaseDataset):
             transform_list.append(transforms.CenterCrop(304))
         
         # blow up (not required)
+        # method=Image.BICUBIC
         # transform_list.append(transforms.Resize([288, 288], method))
         
         return transforms.Compose(transform_list)
 
     def get_transform(self, centerCrop=False):
+        """
+        Transforms the image to a normalized tensor
+        """
         transform_list = []
-
         transform_list += [transforms.ToTensor(),
                            transforms.Normalize((0.5, 0.5, 0.5),
-                                                (0.5, 0.5, 0.5))]
-        
+                                                (0.5, 0.5, 0.5))]        
         return transforms.Compose(transform_list)
     
     def get_rand_upperleft(self):
@@ -164,10 +180,6 @@ class UnalignedDataset(BaseDataset):
         return {'A': A, 'B': B, 'A_paths': A_path, 'B_paths': B_path}
 
     def ld(self, x, y):
-        ''' jitter a bit around '''
-         # resize landmarks accordingly
-        #result = np.random.normal(np.array([x,y]))
-        #return [result[0], result[1]]
         return [float(x), float(y)]
 
     def __len__(self):
