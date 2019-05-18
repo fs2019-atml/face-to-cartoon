c2db848060a325068dfa09bb6d11adbc0905af19
Benjamin Fankhauser <benjamin.fankhauser@bfh.ch>
2019-05-18 11:47:05 +0200

    latest network

--- code/helper_demo/helper.py
+++ code/helper_demo/helper.py
@@ -33,8 +33,7 @@ def to_image(tensor, nrow=8, padding=2,
     grid = torchvision.utils.make_grid(tensor, nrow=nrow, padding=padding, pad_value=pad_value,
                                        normalize=normalize, range=range, scale_each=scale_each)
     # Add 0.5 after unnormalizing to [0, 255] to round to nearest integer
-    ndarr = grid.mul_(255).add_(0.5).clamp_(0, 255).permute(
-        1, 2, 0).to('cpu', torch.uint8).numpy()
+    ndarr = grid.mul_(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).to('cpu', torch.uint8).numpy()
     im = Image.fromarray(ndarr)
     return im
 
